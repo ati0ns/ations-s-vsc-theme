7be1220f0ddc9d7a2377ef681dab409f702629ef
ations <35779030+ati0ns@users.noreply.github.com>
2021-04-05 14:20:53 +0200

    0.0.1

---
+++ preview.js
@@ -0,0 +1,46 @@
+const name = "value";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+const fn = () => undefined;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+let int = 0;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+''
+`${int}`
+
+function run(callback, ...args) {
+    return callback(args);
+}
+
+(callback, ...args) => callback(args);
+
+return;
+
+(callback, ...args) => callback(args);
+
+
+
+if ([].some(e => e)) {
+    console.log(true);
+} else return null;
+
+switch (int) {
+    case 1:
+        int++;
+        break;
+    default:
+        int *= 2;
+        break;
+}
+
+
+
+// Single-line comment
+/**
+ * Multi-line comment
+ */
+
+
+
+{
+    test: Infinity
+}
+(-6.4);
+[1, `2`, three]
